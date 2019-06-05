4029be91658317831c7f2cff9e4621fe79580b20
robert.kuhar <robert.kuhar@climate.com>
2019-06-05 16:01:44 -0700

    Progress through "Create a Gradle build file"

--- src/test/java/org/rekdev/gradle/spring/boot/project/AppTest.java
+++ src/test/java/org/rekdev/gradle/spring/boot/project/AppTest.java
@@ -4,16 +4,19 @@
 package org.rekdev.gradle.spring.boot.project;
 
 import org.testng.annotations.*;
+
 import static org.testng.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+  @Test
+  public void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+  }
 
-    @Test public void testTheTest() {
-        assertEquals(1,1);
-    }
+  @Test
+  public void testTheTest() {
+    assertEquals(1, 1);
+  }
 
 }
